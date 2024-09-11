6ee50b02ddb18b56d4fc8ae92a916587929f02b6
joaodiogo10 <joaodiogohferreira@gmail.com>
2024-09-11 10:40:48 +0100

    node app

---
+++ todo-list/src/app.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({ logger: true });
+
+fastify.get('/', async (request, reply) => {
+  return { message: 'Hello from Fastify and TypeScript!' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+    console.log('Server listening on http://localhost:3000');
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
